63079a871dbcec3e972cdacbad36829632088185
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-18 01:46:21 -0600

    Creación del objeto producto - routing y uso de librería toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 // Componentes
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    CrearProductoComponent,
-    ListarProductosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CrearProductoComponent,
+        ListarProductosComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
     selector: 'app-crear-producto',
@@ -9,9 +12,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CrearProductoComponent implements OnInit {
 
     productoForm: FormGroup;
-    titulo:string="CREAR PRODUCTO"
+    titulo: string = "CREAR PRODUCTO"
 
-    constructor(private fb: FormBuilder) {
+    constructor(private fb: FormBuilder,
+        private router: Router,
+        private toastr: ToastrService) {
         this.productoForm = this.fb.group({
             producto: ['', Validators.required],
             categoria: ['', Validators.required],
@@ -26,6 +31,22 @@ export class CrearProductoComponent implements OnInit {
 
     agregarProducto() {
         console.log(this.productoForm);
+        console.log(this.productoForm.get('producto')?.value);
+
+        const PRODUCTO: Producto = {
+            nombre: this.productoForm.get('producto')?.value,
+            categoria: this.productoForm.get('categoria')?.value,
+            ubicacion: this.productoForm.get('ubicacion')?.value,
+            precio: this.productoForm.get('precio')?.value,
+        }
+
+
+        console.log(PRODUCTO);
+        this.toastr.success(`El producto ' ${PRODUCTO.nombre} ' fue registrado con éxito!`, 'Producto registrado!',{
+            timeOut: 4000,
+        });
+        this.router.navigate(['/']);
+
     }
 
 
