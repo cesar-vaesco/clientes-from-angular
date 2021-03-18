79955b73164b09b1e793ade59748b26fd9012d05
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-18 00:05:01 -0600

    Configurando formulario 'reactivo'

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-crear-producto',
-  templateUrl: './crear-producto.component.html',
-  styleUrls: ['./crear-producto.component.css']
+    selector: 'app-crear-producto',
+    templateUrl: './crear-producto.component.html',
+    styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent implements OnInit {
 
-  constructor() { }
+    productoForm: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor(private fb: FormBuilder) {
+        this.productoForm = this.fb.group({
+            producto: ['', Validators.required],
+            categoria: ['', Validators.required],
+            ubicacion: ['', Validators.required],
+            precio: ['', Validators.required],
+        })
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    agregarProducto() {
+        console.log(this.productoForm);
+    }
 
 }
